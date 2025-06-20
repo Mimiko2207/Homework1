091a6fd4b92c9f7767045eeb92fcc1b1ac6f5073
Mimiko2207 <loparev.m@mail.ru>
2025-06-20 17:36:19 +0700

    Cделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Привет мир");
-
-        System.out.printf("hello world");
+        System.out.printf("Привет мир!");
     }
 }
